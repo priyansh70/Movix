48635e98838271081d461a3e5bb43fcdc866708c
priyansh70 <priyansh70890@gmail.com>
2023-10-16 15:30:30 +0530

    Fix Bug - No Recommendation Movies

--- src/pages/details/carousels/Recommendation.jsx
+++ src/pages/details/carousels/Recommendation.jsx
@@ -1,22 +1,23 @@
-
 import React from "react";
 
 import Carousel from "../../../components/carousel/Carousel";
-import {useFetch} from "../../../hooks/useFetch";
+import { useFetch } from "../../../hooks/useFetch";
 
 const Recommendation = ({ mediaType, id }) => {
-    const { data, loading, error } = useFetch(
-        `/${mediaType}/${id}/recommendations`
-    );
+  const { data, loading, error } = useFetch(
+    `/${mediaType}/${id}/recommendations`
+  );
 
-    return (
-        <Carousel
-            title="Recommendations"
-            data={data?.results}
-            loading={loading}
-            endpoint={mediaType}
-        />
-    );
+  return (
+    data?.length > 0 && (
+      <Carousel
+        title="Recommendations"
+        data={data?.results}
+        loading={loading}
+        endpoint={mediaType}
+      />
+    )
+  );
 };
 
 export default Recommendation;
